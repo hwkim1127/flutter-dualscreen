4c836ec415a3402499b3d0447af449043b99f970
hwkim1127@gmail.com <hwkim1127@gmail.com>
2024-02-23 03:16:40 +0900

    update: java17, namespace added, sdk update, gradle to 8.2 and DSL

--- example/android/app/src/main/kotlin/com/example/dual_screen_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/dual_screen_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.dual_screen_example
+package com.microsoft.dual_screen_example
 
 import io.flutter.embedding.android.FlutterActivity
 
--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.example
+package com.microsoft.dual_screen
 
 import io.flutter.embedding.android.FlutterActivity
 
